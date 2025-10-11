cb2c5326fb021d7f2ca92710ffc4683d1a30d1f6
rashid <rashidrizan8@gamil.com>
2025-10-11 23:33:21 +0530

    added week2 lecture materials

---
+++ week2/week02/week02App.swift
@@ -0,0 +1,17 @@
+//
+//  week02App.swift
+//  week02
+//
+//  Created by Rashid Ahamed on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct week02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ week2/week02Tests/week02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  week02Tests.swift
+//  week02Tests
+//
+//  Created by Rashid Ahamed on 11/10/25.
+//
+
+import Testing
+@testable import week02
+
+struct week02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
