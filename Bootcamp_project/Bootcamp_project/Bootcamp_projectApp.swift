2b2f9be4a580d53f6c350e20b4411222f5a23239
rashid <rashidrizan8@gamil.com>
2025-10-24 10:39:14 +0530

    added bootcamp project

---
+++ Bootcamp_project/Bootcamp_project/Bootcamp_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bootcamp_projectApp.swift
+//  Bootcamp_project
+//
+//  Created by Rashid Ahamed on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Bootcamp_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            IntroView()
+        }
+    }
+}
